84ad7fa022945433decf7440548c1580859361dd
Stuart Urback <stuart@urback.net>
2019-07-17 10:35:03 -0600

    Update packages, make the seed file not fail, update the readme a bit

--- seeds/base.js
+++ seeds/base.js
@@ -1,42 +1,46 @@
-
 exports.seed = function(knex, Promise) {
-  return knex('transactions').del()
-    .then(function () {
-      return knex('teams').insert([
+  return knex("transactions")
+    .del()
+    .then(function() {
+      return knex("teams").del();
+    })
+    .then(function() {
+      return knex("teams").insert([
         {
           id: 1,
-          created_at: new Date(), 
-          updated_at: new Date(), 
-          name: 'Tesla'
+          created_at: new Date(),
+          updated_at: new Date(),
+          name: "Tesla"
         },
         {
           id: 2,
-          created_at: new Date(), 
+          created_at: new Date(),
           updated_at: new Date(),
-          name: 'Uber'
+          name: "Uber"
         }
       ]);
-    }).then(function () {
-      return knex('transactions').insert([
+    })
+    .then(function() {
+      return knex("transactions").insert([
         {
-          created_at: new Date(), 
-          updated_at: new Date(), 
-          amount: 36375, 
-          description: 'Flight from EWR to SFO',
+          created_at: new Date(),
+          updated_at: new Date(),
+          amount: 36375,
+          description: "Flight from EWR to SFO",
           team_id: 1
         },
         {
-          created_at: new Date(), 
-          updated_at: new Date(), 
-          amount: 1264, 
-          description: 'Uber Ride in LA',
+          created_at: new Date(),
+          updated_at: new Date(),
+          amount: 1264,
+          description: "Uber Ride in LA",
           team_id: 1
         },
         {
-          created_at: new Date(), 
-          updated_at: new Date(), 
-          amount: 73821, 
-          description: 'Stay at the Hilton SF',
+          created_at: new Date(),
+          updated_at: new Date(),
+          amount: 73821,
+          description: "Stay at the Hilton SF",
           team_id: 2
         }
       ]);
